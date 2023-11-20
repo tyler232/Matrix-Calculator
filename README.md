9b49c09d6e037e01b8a43762f3f0bf351a1b3bb4
Tyler Liu <cliu232@illinois.edu>
2023-11-19 21:30:01 -0600

    readme tutorial part1

--- README.md
+++ README.md
@@ -43,3 +43,102 @@ Specific Usage Please see tutorial below
 If you encountered bugs with the project please make a Issue Post or E-mail me at "lchangbo1227@gmail.com"
 
 ## Tutorial
+
+### Create Matrix
+```{C++}
+tiele::Matrix matrix_A({{3, 1, 4},
+                        {2, 3, 6},
+                        {0, 5, 10}});
+std::cout << matrix_A << std::endl;
+```
+```{txt}
+[[3,    1,      4]
+[2,     3,      6]
+[0,     5,      10]]
+Rows: 3
+Cols: 3
+```
+
+### Create Zero Matrix
+```{C++}
+// First parameter is amount of rows, second parameter is amount of columns
+tiele::Matrix matrix_B(3, 3);
+std::cout << matrix_B << std::endl;
+```
+```{txt}
+[[0,    0,      0]
+[0,     0,      0]
+[0,     0,      0]]
+Rows: 3
+Cols: 3
+```
+
+### Create Identity Matrix
+```{C++}
+// Parameter is the size of identity
+tiele::Matrix I = tiele::identity(3);
+std::cout << I << std::endl;
+```
+```{txt}
+[[1,    0,      0]
+[0,     1,      0]
+[0,     0,      1]]
+Rows: 3
+Cols: 3
+```
+
+### Get and Set Value in the Matrix
+```{C++}
+tiele::Matrix A({{3, 1, 4},
+                {2, 3, 6},
+                {0, 5, 10}});
+// get value at row 2 col 1
+double val = A.getValue(2, 1);
+std::cout << val << std::endl;
+// set value at row 2 col 1 to -20
+A.setValue(2, 1, -20);
+val = A.getValue(2, 1);
+std::cout << val << std::endl;
+```
+```{txt}
+5
+-20
+```
+
+### Basic Matrix Operation
+```{C++}
+tiele::Matrix A({{3, 1, 4},
+                {2, 3, 6},
+                {0, 5, 10}});
+tiele::Matrix B({{2, 4, -1},
+                {3, -2, 7.5},
+                {9, 5, 1}});
+std::cout << "Addition" << std::endl;
+std::cout << A + B << std::endl;
+std::cout << "Subtraction" << std::endl;
+std::cout << A - B << std::endl;
+std::cout << "Multiplication" << std::endl;
+std::cout << A * B << std::endl;
+```
+```{txt}
+Addition
+[[5,    5,      3]
+[5,     1,      13.5]
+[9,     10,     11]]
+Rows: 3
+Cols: 3
+
+Subtraction
+[[1,    -3,     5]
+[-1,    5,      -1.5]
+[-9,    0,      9]]
+Rows: 3
+Cols: 3
+
+Multiplication
+[[45,   30,     8.5]
+[67,    32,     26.5]
+[105,   40,     47.5]]
+Rows: 3
+Cols: 3
+```
--- main.cpp
+++ main.cpp
@@ -14,91 +14,16 @@ int main() {
                                                {7}};
     std::vector<std::vector<double>> data_a = {{2, -2},
                                                {4, 3}};
-    tiele::Matrix matrix_A(data_A);
-    tiele::Matrix matrix_B(data_B);
-    tiele::Matrix matrix_b(data_b);
-    tiele::Matrix matrix_a(data_a);
-
-    // tiele::Matrix matrix_C = tiele::addition(matrix_A, matrix_B);
-    // std::cout << matrix_C << std::endl;
-    // std::cout << std::endl;
-    // tiele::Matrix matrix_D = tiele::subtraction(matrix_A, matrix_B);
-    // std::cout << matrix_D << std::endl;
-    // std::cout << std::endl;
-    // tiele::Matrix matrix_E = tiele::multiplication(matrix_A, matrix_B);
-    // std::cout << matrix_E << std::endl;
-    // std::cout << std::endl;
-    tiele::Matrix matrix_F = tiele::transpose(matrix_A);
-    std::cout << matrix_F << std::endl;
-    std::cout << std::endl;
-    tiele::Matrix matrix_G = tiele::identity(3);
-    std::cout << matrix_G << std::endl;
-    std::cout << std::endl;
-
-    tiele::Matrix matrix_H = tiele::inverse(matrix_A);
-    std::cout << matrix_H << std::endl;
-    std::cout << std::endl;
-
-    tiele::Matrix matrix_I = tiele::solve(matrix_A, matrix_b);
-    std::cout << matrix_I << std::endl;
-    std::cout << std::endl;
-
-    tiele::Matrix matrix_J = tiele::reduced_row_echelon(matrix_B);
-    std::cout << matrix_J << std::endl;
-    std::cout << std::endl;
-
-    int r = tiele::matrix_rank(matrix_B);
-    std::cout << r << std::endl;
-    std::cout << std::endl;
-
-    double d = tiele::det(matrix_a);
-    std::cout << d << std::endl;
-    std::cout << std::endl;
-
-    double t = tiele::trace(matrix_A);
-    std::cout << t << std::endl;
-    std::cout << std::endl;
-
-    double n = tiele::norm(matrix_A, std::numeric_limits<int>::infinity());
-    std::cout << "Norm " << n << std::endl;
-    std::cout << std::endl;
-
-    double c = tiele::cond(matrix_A, std::numeric_limits<int>::infinity());
-    std::cout << "Cond " << c << std::endl;
-    std::cout << std::endl;
-
-    std::cout << tiele::qrDecomposition(matrix_A).first << std::endl;
-    std::cout << tiele::qrDecomposition(matrix_A).second << std::endl;
-
-
-    std::cout << tiele::eigenvalues(matrix_A)[2] << std::endl;
-    std::cout << tiele::eigenvectors(matrix_A)[0] << std::endl;
-
-    std::cout << std::endl;
-    std::cout << tiele::norm(matrix_A, 2) << std::endl;
-    std::cout << tiele::cond(matrix_A, 2) << std::endl;
-
-    std::cout << std::endl;
-    std::cout << tiele::luDecomposition(matrix_A).first << std::endl;
-    std::cout << tiele::luDecomposition(matrix_A).second << std::endl;
-
-    std::cout << tiele::eigenvectors_asMatrix(matrix_A) << std::endl;
-
-    std::cout << tiele::eigenvectors_normalized(matrix_A)[2] << std::endl;
-    std::cout << tiele::eigenvectors_normalized_asMatrix(matrix_A) << std::endl;
-
-    std::cout << tiele::eigen(matrix_A).second << std::endl;
-
-    std::cout << tiele::svd(matrix_A)[0] << std::endl;
-
-    std::cout << tiele::svd(matrix_A)[2] << std::endl;
-
-    std::cout << tiele::svd(matrix_A)[1] << std::endl;
-
-    std::cout << tiele::inner(tiele::transpose(tiele::Matrix({1, 2, 4})), 
-                            tiele::transpose(tiele::Matrix({3, 2, 8}))) << std::endl;
-    
-    tiele::Matrix matrix_LL = tiele::reduced_row_echelon(matrix_A);
-    std::cout << matrix_LL << std::endl;
+    tiele::Matrix A({{3, 1, 4},
+                    {2, 3, 6},
+                    {0, 5, 10}});
+    tiele::Matrix B({{2, 4, -1},
+                    {3, -2, 7.5},
+                    {9, 5, 1}});
+    double val = A.getValue(2, 1);
+    std::cout << val << std::endl;
+    A.setValue(2, 1, -20);
+    val = A.getValue(2, 1);
+    std::cout << val << std::endl;
     return 0;
 }
